a98676065e9b19bc76cb9cb41267611fa737b855
Test <krishpatel8463@gmail.com>
2024-11-01 13:51:52 +0530

    fetched data from api

---
+++ app.js
@@ -0,0 +1,10 @@
+const url = "http://api.weatherapi.com/v1/current.json?key=da69256eeede4b40a12145258243110&q=London&aqi=yes"
+
+const getWeatherData = async () => {
+    const data = await fetch(url);
+    const temp = await data.json();
+    console.log(temp);
+    console.log(temp.location.name);
+}
+
+getWeatherData();
